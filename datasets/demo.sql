46d72749c21b162908799ec0a557301cc4816f32
Jason Lantz <jlantz@salesforce.com>
2019-02-03 23:04:36 -0600

    Update to use new bulkdata enhancements in CumulusCI

---
+++ datasets/demo.sql
@@ -0,0 +1,17 @@
+BEGIN TRANSACTION;
+CREATE TABLE accounts (
+	id INTEGER NOT NULL, 
+	name VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "accounts" VALUES(1,'Demo Account 1');
+INSERT INTO "accounts" VALUES(2,'Demo Account 2');
+CREATE TABLE cumulusci_demo (
+	id INTEGER NOT NULL, 
+	name VARCHAR(255), 
+	account__c VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "cumulusci_demo" VALUES(1,'Demo 1','1');
+INSERT INTO "cumulusci_demo" VALUES(2,'Demo 2','2');
+COMMIT;
